d0d4bcb1c0fe8bc72f2531fdee3c6941534cd86d
khushichoudhary29 <khushichoudhary2109@gmail.com>
2026-02-10 22:49:54 +0530

    Added SQLite database setup using SQLAlchemy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.core.database import Base, engine
 
 app = FastAPI(title="Secure Data Classification System")
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
